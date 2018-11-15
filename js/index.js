d408a5afa4c2b7e5e08a88b08807e4d109b03317
Ryan <ryangeorgebarker0@gmail.com>
2018-11-15 11:11:43 +0000

    added popular playlists

--- js/index.js
+++ js/index.js
@@ -15,14 +15,11 @@ let currentUser = {
     username: ''
 }
 
-const localPlaylists = {
-    playlists: []
-}
-
+let localPlaylists = []
 
 const createPlaylistHeader = () => {
     const topPlaylistHeader = document.createElement('h2')
-    topPlaylistHeader.innerText = 'Top Playlists'
+    topPlaylistHeader.innerText = 'Popular Playlists'
     topPlaylistHeader.id = 'playlist-header'
     contentContainer.appendChild(topPlaylistHeader)
 }
@@ -34,34 +31,24 @@ const createPlaylistList = () => {
 }
 
 //WIP
-const renderUserPlaylists = (user) => {
+const renderTopPlaylist = playlist => {
     const playlistContainer = document.getElementById('playlist-list')
-    user.playlists.forEach(playlist => {
-        playlist
-        const playlistElement = document.createElement('div')
-        playlistElement.dataset.id = `${user.id}`
-        playlistElement.innerHTML = `
-        
-
+    const playlistElement = document.createElement('div')
+    playlistElement.innerHTML = `
 <div class="card text-white bg-dark mb-3" style="max-width: 20rem;">
-  <div class="card-header">${user.username}'s Playlist</div>
   <div class="card-body">
     <h4 class="card-title">${playlist.name}</h4>
     <p class="card-text">${playlist.name}</p>
   </div>
 </div>
-        
-        
-        
         `
         playlistContainer.appendChild(playlistElement)
-    })
 }
 
-const renderAllUserPlaylists = users => {
+const renderTopPlaylists = playlists => {
     createPlaylistHeader()
     createPlaylistList()
-    users.forEach(user => renderUserPlaylists(user))
+    playlists.forEach(playlist => renderTopPlaylist(playlist))
 }
 
 const activateHomepageButton = () => {
@@ -77,11 +64,13 @@ const renderHomepage = () => {
     getUsers()
         .then(users => {
             state.users = users
-            renderAllUserPlaylists(state.users)
+    getPlaylists()
+        .then(playlists => {
+            localPlaylists = playlists
+            renderTopPlaylists(getArrayOfPopularPlaylists())
         })
+})
 }
-
-renderHomepage()
 
 const findCurrentUserInState = () => state.users.find(user => user.id === currentUser.id)
 
@@ -117,3 +106,42 @@ const createRecentSongsHeader = () => {
     recentSongsHeader.id = 'recent-songs-header'
     contentContainer.appendChild(recentSongsHeader)
 }
+
+// Find and Render Popular Playlists
+
+const findPlaylistLocally = playlistId => localPlaylists.find(playlist => playlist.id === playlistId)
+
+const getPlaylistIdsFromUsers = () =>
+    state.users.map(user =>
+        user.playlists.map(playlist =>
+            playlist.id)).flat()
+
+const getArrayOfPopularPlaylistIds = () => 
+    sortByPopularAndRemoveDuplicates(getPlaylistIdsFromUsers()).map(element =>
+        parseInt(element))
+
+const getArrayOfPopularPlaylists = () => getArrayOfPopularPlaylistIds().map(element => findPlaylistLocally(element))
+
+const sortByPopularAndRemoveDuplicates = array => {
+    var frequency = {},
+        value;
+    for (var i = 0; i < array.length; i++) {
+        value = array[i];
+        if (value in frequency) {
+            frequency[value]++;
+        } else {
+            frequency[value] = 1;
+        }
+    }
+    var uniques = [];
+    for (value in frequency) {
+        uniques.push(value);
+    }
+
+    function compareFrequency(a, b) {
+        return frequency[b] - frequency[a];
+    }
+    return uniques.sort(compareFrequency);
+}
+
+renderHomepage()
